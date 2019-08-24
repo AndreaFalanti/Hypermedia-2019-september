ab28e688659e31b362703e8a773e3c3213ce53ef
Andrea Falanti <a.falanti@gmail.com>
2019-08-24 16:37:17 +0200

    Fix and improve event reservation script and page

--- public/assets/js/reservation.js
+++ public/assets/js/reservation.js
@@ -1,8 +1,20 @@
+// Add event data to input tags
 function addEventData() {
     $(document).ready(() => {
-        $("#inputEventName").val("Event 1");
-        $("#inputEventLocation").val("Milano");
-        $("#inputEventDate").val("21-08-19");
+        const urlParams = new URLSearchParams(window.location.search);
+        let idValue = urlParams.get("id");
+        if (idValue) {
+            fetch('/v2/events/' + idValue).then(r => r.json())
+                .then(event => {
+                    $("#inputEventName").val(event.name);
+                    $("#inputEventLocation").val(event.location);
+                    $("#inputEventDate").val(new Date(event.date).toDateString());
+                });
+        }
+        else {
+            console.log("id not present");
+            // redirect to another page?
+        }
     });
 }
 
